e6e6ebecf3ce87110f8e96c3ea16c21f1dcb39f9
Parjanya Vyas <40213098+parjanya-vyas@users.noreply.github.com>
2018-07-29 16:02:40 +0530

    Introducing proper inheritance and removing obsolete structs.

--- Graph_Creator/graph_creator_verifier.cpp
+++ Graph_Creator/graph_creator_verifier.cpp
@@ -1,52 +1,99 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 #define INVERT(N) 1-N
+
+using namespace std;
 
 typedef unsigned long long DAMN_LONG;
 
-typedef struct bit_ot_node {
-    int a,b;
-    list<struct bit_ot_node *> ngb;
-} BIT_OT_NODE;
+class Crypto_node {
+	public:
+	DAMN_LONG *random_vars;
+	list<Crypto_node *> ngb;
+};
 
-typedef struct field_ole_node {
-    DAMN_LONG a,b;
-    list<struct field_ole_node *> ngb;
-} FIELD_OLE_NODE;
+class Crypto_graph {
+	
+	protected:
 
-typedef struct beaver_triple_node {
-    DAMN_LONG a,b,c;
-    list<struct beaver_triple_node *> ngb;
-} BEAVER_TRIPLE_NODE;
-
-class Bit_OT_graph {
-
-    BIT_OT_NODE bit_ot_nodes[2][2][2];
-
-    public:
-
-    void init_bit_ot_node(BIT_OT_NODE *n, int a, int b) {
-        n->a = a;
-        n->b = b;
+	DAMN_LONG field_sz;
+	int num_random_vars;
+	
+	public:
+	
+	DAMN_LONG field_plus(DAMN_LONG a, DAMN_LONG b) {
+        return (a+b)%field_sz;
+    }
+	
+	DAMN_LONG field_minus(DAMN_LONG a, DAMN_LONG b) {
+        return (a-b)%field_sz;
     }
 
-    void init_all_bit_ot_nodes() {
-        for(int i=0;i<2;i++) {
-            for(int j=0;j<2;j++) {
-                init_bit_ot_node(&bit_ot_nodes[i][j][0], i, j);
-                init_bit_ot_node(&bit_ot_nodes[i][j][1], i, j);
+    DAMN_LONG field_multiply(DAMN_LONG a, DAMN_LONG b) {
+        return (a*b)%field_sz;
+    }
+    
+    void init_single_node(Crypto_node *n, DAMN_LONG val[]) {
+    	for(int i=0;i<num_random_vars;i++)
+    		n->random_vars[i] = val[i];
+    }
+    
+    virtual void allocate_nodes_memory() = 0;
+    virtual void init_all_nodes() = 0;
+    
+	void print_neighbours(Crypto_node *node) {
+		
+        cout << "Number of neighbours: " << node->ngb.size() << endl;
+        list<Crypto_node *> :: iterator it;
+        for(it = node->ngb.begin(); it!=node->ngb.end(); it++) {
+        	cout << "(" << (*it)->random_vars[0];
+        	for(int i=1;i<num_random_vars;i++)
+            	cout << ", " << (*it)->random_vars[i];
+            cout << ")" << endl;
+		}
+	}
+    
+    Crypto_graph(DAMN_LONG f_sz, int n_rand_vars) {
+        field_sz = f_sz;
+        num_random_vars = n_rand_vars;
+	}
+};
+
+class Bit_OT_graph : Crypto_graph {
+
+    Crypto_node ***bit_ot_nodes;
+
+    public:
+    	
+    void allocate_nodes_memory() {
+    	bit_ot_nodes = new Crypto_node**[field_sz];
+        for(DAMN_LONG i=0;i<field_sz;i++) {
+            bit_ot_nodes[i] = new Crypto_node*[field_sz];
+            for(DAMN_LONG j=0;j<field_sz;j++) {
+                bit_ot_nodes[i][j] = new Crypto_node[2];
+                bit_ot_nodes[i][j][0].random_vars = new DAMN_LONG[num_random_vars];
+                bit_ot_nodes[i][j][1].random_vars = new DAMN_LONG[num_random_vars];
+            }
+        }
+	}
+
+    void init_all_nodes() {
+        for(int i=0;i<field_sz;i++) {
+            for(int j=0;j<field_sz;j++) {
+            	DAMN_LONG vals[2] = {i, j};
+                init_single_node(&bit_ot_nodes[i][j][0], vals);
+                init_single_node(&bit_ot_nodes[i][j][1], vals);
             }
         }
     }
 
-    Bit_OT_graph() {
-        init_all_bit_ot_nodes();
+    Bit_OT_graph() : Crypto_graph(2, 2) {
+        allocate_nodes_memory();
+        init_all_nodes();
         for(int i=0;i<2;i++) {
             for(int j=0;j<2;j++) {
-                int s1 = bit_ot_nodes[i][j][0].a;
-                int s2 = bit_ot_nodes[i][j][0].b;
+                int s1 = bit_ot_nodes[i][j][0].random_vars[0];
+                int s2 = bit_ot_nodes[i][j][0].random_vars[1];
                 for(int c=0;c<2;c++) {
                     int s = c==0 ? s1 : s2;
                     bit_ot_nodes[s1][s2][0].ngb.push_back(&bit_ot_nodes[s][c][1]);
@@ -55,62 +102,47 @@ class Bit_OT_graph {
             }
         }
     }
-
-    void print_bit_ot_neighbours(int a, int b, int side) {
-        BIT_OT_NODE node = bit_ot_nodes[a][b][side];
-        cout << "Number of neighbours: " << node.ngb.size() << endl;
-        list<BIT_OT_NODE *> :: iterator it;
-        for(it = node.ngb.begin(); it!=node.ngb.end(); it++)
-            cout << "(" << (*it)->a << ", " << (*it)->b << ")" << endl;
+    
+    void print_neighbours(DAMN_LONG a, DAMN_LONG b, DAMN_LONG side) {
+    	Crypto_graph::print_neighbours(&bit_ot_nodes[a][b][side]);
     }
 };
 
-class Field_OLE_graph {
+class Field_OLE_graph : public Crypto_graph {
 
-    FIELD_OLE_NODE ***field_ole_nodes;
-    DAMN_LONG field_sz;
+    Crypto_node ***field_ole_nodes;
 
     public:
 
-    DAMN_LONG field_minus(DAMN_LONG a, DAMN_LONG b) {
-        return (a-b)%field_sz;
-    }
-
-    DAMN_LONG field_multiply(DAMN_LONG a, DAMN_LONG b) {
-        return (a*b)%field_sz;
-    }
-
-    void allocate_field_ole_nodes_memory() {
-        field_ole_nodes = new FIELD_OLE_NODE**[field_sz];
+    void allocate_nodes_memory() {
+        field_ole_nodes = new Crypto_node**[field_sz];
         for(DAMN_LONG i=0;i<field_sz;i++) {
-            field_ole_nodes[i] = new FIELD_OLE_NODE*[field_sz];
-            for(DAMN_LONG j=0;j<field_sz;j++)
-                field_ole_nodes[i][j] = new FIELD_OLE_NODE[2];
-        }
-    }
-
-    void init_field_ole_node(FIELD_OLE_NODE *n, DAMN_LONG a, DAMN_LONG b) {
-        n->a = a;
-        n->b = b;
-    }
-
-    void init_all_field_ole_nodes() {
-        for(DAMN_LONG i=0;i<field_sz;i++) {
+            field_ole_nodes[i] = new Crypto_node*[field_sz];
             for(DAMN_LONG j=0;j<field_sz;j++) {
-                init_field_ole_node(&field_ole_nodes[i][j][0], i, j);
-                init_field_ole_node(&field_ole_nodes[i][j][1], i, j);
+                field_ole_nodes[i][j] = new Crypto_node[2];
+                field_ole_nodes[i][j][0].random_vars = new DAMN_LONG[num_random_vars];
+                field_ole_nodes[i][j][1].random_vars = new DAMN_LONG[num_random_vars];
             }
         }
     }
 
-    Field_OLE_graph(DAMN_LONG f_sz) {
-        field_sz = f_sz;
-        allocate_field_ole_nodes_memory();
-        init_all_field_ole_nodes();
+    void init_all_nodes() {
         for(DAMN_LONG i=0;i<field_sz;i++) {
             for(DAMN_LONG j=0;j<field_sz;j++) {
-                DAMN_LONG a = field_ole_nodes[i][j][0].a;
-                DAMN_LONG p = field_ole_nodes[i][j][0].b;
+            	DAMN_LONG vals[2] = {i, j};
+                init_single_node(&field_ole_nodes[i][j][0], vals);
+                init_single_node(&field_ole_nodes[i][j][1], vals);
+            }
+        }
+    }
+
+    Field_OLE_graph(DAMN_LONG f_sz) : Crypto_graph(f_sz, 2) {
+    	allocate_nodes_memory();
+        init_all_nodes();
+        for(DAMN_LONG i=0;i<field_sz;i++) {
+            for(DAMN_LONG j=0;j<field_sz;j++) {
+                DAMN_LONG a = field_ole_nodes[i][j][0].random_vars[0];
+                DAMN_LONG p = field_ole_nodes[i][j][0].random_vars[1];
                 for(DAMN_LONG q=0;q<field_sz;q++) {
                     DAMN_LONG b = field_minus(field_multiply(p,q),a);
                     field_ole_nodes[a][p][0].ngb.push_back(&field_ole_nodes[b][q][1]);
@@ -119,74 +151,54 @@ class Field_OLE_graph {
             }
         }
     }
-
-    void print_field_ole_neighbours(DAMN_LONG a, DAMN_LONG b, DAMN_LONG side) {
-        FIELD_OLE_NODE node = field_ole_nodes[a][b][side];
-        cout << "Number of neighbours: " << node.ngb.size() << endl;
-        list<FIELD_OLE_NODE *> :: iterator it;
-        for(it = node.ngb.begin(); it!=node.ngb.end(); it++)
-            cout << "(" << (*it)->a << ", " << (*it)->b << ")" << endl;
+    
+    void print_neighbours(DAMN_LONG a, DAMN_LONG b, DAMN_LONG side) {
+    	Crypto_graph::print_neighbours(&field_ole_nodes[a][b][side]);
     }
 };
 
-class Beaver_Triples_graph {
+class Beaver_Triples_graph : Crypto_graph {
 
-    BEAVER_TRIPLE_NODE ****beaver_triple_nodes;
-    DAMN_LONG field_sz;
+    Crypto_node ****beaver_triple_nodes;
 
     public:
 
-    DAMN_LONG field_minus(DAMN_LONG a, DAMN_LONG b) {
-        return (a-b)%field_sz;
-    }
-
-    DAMN_LONG field_plus(DAMN_LONG a, DAMN_LONG b) {
-        return (a+b)%field_sz;
-    }
-
-    DAMN_LONG field_multiply(DAMN_LONG a, DAMN_LONG b) {
-        return (a*b)%field_sz;
-    }
-
-    void allocate_beaver_triple_nodes_memory() {
-        beaver_triple_nodes = new BEAVER_TRIPLE_NODE***[field_sz];
+    void allocate_nodes_memory() {
+        beaver_triple_nodes = new Crypto_node***[field_sz];
         for(DAMN_LONG i=0;i<field_sz;i++) {
-            beaver_triple_nodes[i] = new BEAVER_TRIPLE_NODE**[field_sz];
+            beaver_triple_nodes[i] = new Crypto_node**[field_sz];
             for(DAMN_LONG j=0;j<field_sz;j++) {
-                beaver_triple_nodes[i][j] = new BEAVER_TRIPLE_NODE*[field_sz];
-                for(DAMN_LONG k=0;k<field_sz;k++)
-                    beaver_triple_nodes[i][j][k] = new BEAVER_TRIPLE_NODE[2];
-            }
-        }
-    }
-
-    void init_beaver_triple_node(BEAVER_TRIPLE_NODE *n, DAMN_LONG a, DAMN_LONG b, DAMN_LONG c) {
-        n->a = a;
-        n->b = b;
-        n->c = c;
-    }
-
-    void init_all_beaver_triple_nodes() {
-        for(DAMN_LONG i=0;i<field_sz;i++) {
-            for(DAMN_LONG j=0;j<field_sz;j++) {
+                beaver_triple_nodes[i][j] = new Crypto_node*[field_sz];
                 for(DAMN_LONG k=0;k<field_sz;k++) {
-                    init_beaver_triple_node(&beaver_triple_nodes[i][j][k][0], i, j, k);
-                    init_beaver_triple_node(&beaver_triple_nodes[i][j][k][1], i, j, k);
+                    beaver_triple_nodes[i][j][k] = new Crypto_node[2];
+                    beaver_triple_nodes[i][j][k][0].random_vars = new DAMN_LONG[num_random_vars];
+                	beaver_triple_nodes[i][j][k][1].random_vars = new DAMN_LONG[num_random_vars];
                 }
             }
         }
     }
 
-    Beaver_Triples_graph(DAMN_LONG f_sz) {
-        field_sz = f_sz;
-        allocate_beaver_triple_nodes_memory();
-        init_all_beaver_triple_nodes();
+    void init_all_nodes() {
         for(DAMN_LONG i=0;i<field_sz;i++) {
             for(DAMN_LONG j=0;j<field_sz;j++) {
                 for(DAMN_LONG k=0;k<field_sz;k++) {
-                    DAMN_LONG a1 = beaver_triple_nodes[i][j][k][0].a;
-                    DAMN_LONG b1 = beaver_triple_nodes[i][j][k][0].b;
-                    DAMN_LONG c1 = beaver_triple_nodes[i][j][k][0].c;
+                	DAMN_LONG vals[3] = {i, j, k};
+	                init_single_node(&beaver_triple_nodes[i][j][k][0], vals);
+	                init_single_node(&beaver_triple_nodes[i][j][k][1], vals);
+                }
+            }
+        }
+    }
+
+    Beaver_Triples_graph(DAMN_LONG f_sz) : Crypto_graph(f_sz, 3) {
+        allocate_nodes_memory();
+        init_all_nodes();
+        for(DAMN_LONG i=0;i<field_sz;i++) {
+            for(DAMN_LONG j=0;j<field_sz;j++) {
+                for(DAMN_LONG k=0;k<field_sz;k++) {
+                    DAMN_LONG a1 = beaver_triple_nodes[i][j][k][0].random_vars[0];
+                    DAMN_LONG b1 = beaver_triple_nodes[i][j][k][0].random_vars[1];
+                    DAMN_LONG c1 = beaver_triple_nodes[i][j][k][0].random_vars[2];
                     for(DAMN_LONG b2=0;b2<field_sz;b2++) {
                         for(DAMN_LONG c2=0;c2<field_sz;c2++) {
                             DAMN_LONG a2 = field_minus(field_multiply(field_plus(b1,b2),field_plus(c1,c2)),a1);
@@ -198,13 +210,9 @@ class Beaver_Triples_graph {
             }
         }
     }
-
-    void print_beaver_triple_neighbours(DAMN_LONG a, DAMN_LONG b, DAMN_LONG c, DAMN_LONG side) {
-        BEAVER_TRIPLE_NODE node = beaver_triple_nodes[a][b][c][side];
-        cout << "Number of neighbours: " << node.ngb.size() << endl;
-        list<BEAVER_TRIPLE_NODE *> :: iterator it;
-        for(it = node.ngb.begin(); it!=node.ngb.end(); it++)
-            cout << "(" << (*it)->a << ", " << (*it)->b << ", " << (*it)->c << ")" << endl;
+    
+    void print_neighbours(DAMN_LONG a, DAMN_LONG b, DAMN_LONG c, DAMN_LONG side) {
+    	Crypto_graph::print_neighbours(&beaver_triple_nodes[a][b][c][side]);
     }
 };
 
@@ -221,7 +229,7 @@ int main() {
             return 0;
         cin >> a >> b;
         while(side!=-1 && a!=-1 && b!=-1) {
-            ot_graph.print_bit_ot_neighbours(a, b, side);
+            ot_graph.print_neighbours(a, b, side);
             cin >> side; 
             if(side==-1)
                 return 0;
@@ -239,7 +247,7 @@ int main() {
             return 0;
         cin >> a >> b;
         while(side!=-1 && a!=-1 && b!=-1) {
-            ole_graph.print_field_ole_neighbours(a, b, side);
+            ole_graph.print_neighbours(a, b, side);
             cin >> side; 
             if(side==-1)
                 return 0;
@@ -257,7 +265,7 @@ int main() {
             return 0;
         cin >> a >> b >> c;
         while(side!=-1 && a!=-1 && b!=-1 && c!=-1) {
-            triples_graph.print_beaver_triple_neighbours(a, b, c, side);
+            triples_graph.print_neighbours(a, b, c, side);
             cin >> side; 
             if(side==-1)
                 return 0;
